5d47ba49b5f6e218b5d760d85e980736bfc397f5
Murphy Leung <1025623017@qq.com>
2016-05-01 17:05:44 +0800

    coffeeScript testing
    coffeeScript testing

---
+++ js/test.js
@@ -0,0 +1,43 @@
+
+/*
+square = (x) -> x * x
+cube = (x) -> square(x) * x
+myName = "Murphy"
+fill = (head, text = "nothing") ->
+  "filling the #{head} with #{text} ..."
+song = [
+  "do", "re", "mi", "fa", "so"
+]
+singers = {
+  leung: "rock",
+  lee: "roll"
+}
+kids =
+  elder:
+    name: "leung"
+    age: 11
+  younger:
+    name: "jenny"
+    age: 8
+$("#header").attr
+  "class": "on"
+console.log fill(myName)
+outer = 1
+changeNumber = ->
+  inner = 24
+  outer = 0
+inner = changeNumber()
+singing = happy = high = goHome = null
+myName = "Jenny" if singing?
+if happy and high
+  clap()
+  hahaha()
+else
+  goHome
+date = if "friday" then "yep" else "nup"
+$("h1").click ->
+  $(this).css
+    "border": "1px red solid"
+  alert("我是h1！")
+ */
+
