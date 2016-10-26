ee3ea7939d5686dd381120bd44e1bb9dee72fd8e
murphy <1025623017@qq.com>
2016-10-26 09:46:34 +0800

    add Vue

--- js/comm.js
+++ js/comm.js
@@ -1,33 +1,36 @@
-$(function() {
+var index = new Vue({
+  el: '#index',
+  data: {
+    ex: '123'
+  },
+  methods: {
+    init: function() {
+      //返回顶部
+      var timer_toTop=null;
+      var timer_toContent=null;
+      var timer_toContent2=null;
 
+      $("#toTop").on('click',function() {
+        timer_toTop=setInterval(function() {
+          if (document.body.scrollTop>0) {/*Chrome*/
+            document.body.scrollTop-=90;
+          }else if(document.documentElement.scrollTop>0) {/*Firefox*/
+            document.documentElement.scrollTop-=90;
+          }else{
+            clearInterval(timer_toTop);
+          }
+        },10);
+      });
 
-
-  //返回顶部
-  var timer_toTop=null;
-  var timer_toContent=null;
-  var timer_toContent2=null;
-
-  $("#toTop").click(function() {
-    timer_toTop=setInterval(function() {
-      if (document.body.scrollTop>0) {/*Chrome*/
-        document.body.scrollTop-=90;
-      }else if(document.documentElement.scrollTop>0) {/*Firefox*/
-        document.documentElement.scrollTop-=90;
-      }else{
-        clearInterval(timer_toTop);
-      }
-    },10);
-  });
-
-  $.ajax({
-    url: '/blog/db/db',
-    type: 'GET',
-    success: function(rsp) {
-      eval('var db = ' + rsp)
-      console.log(db);
+      //获取数据库
+      $.ajax({
+        url: '/blog/db/db',
+        type: 'GET',
+        success: function(rsp) {
+          eval('var db = ' + rsp)
+          console.log(db);
+        }
+      })
     }
-  })
-
-
-
-});
+  }
+})
