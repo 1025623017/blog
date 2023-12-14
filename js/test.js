e87b4decc159e32fb4686d372f6a1f2588b811da
Murphy Leung <1025623017@qq.com>
2023-12-15 00:12:20 +0800

    2023年12月15日 00:12:17
    2023年12月15日 00:12:17

--- js/test.js
+++ js/test.js
@@ -1,1 +1,25 @@
+var test = new Vue({
+  el: '#index',
+  data: {
+  	excel_list:{
+  		a:1,
+  		b:2
+  	},
+  	ppt_list:{}
+  },
+  methods: {
 
+    getData: function(a){
+      console.log(test.excel_list);
+      console.log("loading excel data...");
+    },
+
+    postData: function(b){
+      console.log(test.ppt_list);
+      console.log("loading ppt 01...");
+    }
+
+  }
+})
+
+test.getData();
