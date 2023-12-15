28c25e1fcb031d75dd9a2be326287a259ef52aac
Murphy Leung <1025623017@qq.com>
2023-12-16 00:13:03 +0800

    2023年12月16日 00:13:00
    2023年12月16日 00:13:00

--- js/comm.js
+++ js/comm.js
@@ -4,26 +4,23 @@ var index = new Vue({
     //空的本地数据库
     db: {},
     //获取Ajax数据库失败标识
-    db_empty: true
+    db_empty: true,
+    db_rsp:{}
   },
   methods: {
     init: function() {
-
-
-
 
       $.ajax({
         url: 'db/db',
         type: 'GET',
         async: false,
-        //获取网络数据库
         success: function(rsp) {
           eval('index.db = ' + rsp);
           index.db_empty = false;
           console.log('Database Github Version loaded successful');
         },
-        //获取本地数据库
         error: function(rsp) {
+          console.log('DB loaded fail from Github');
           $.ajax({
             url: 'https://1025623017.github.io/blog/db/db',
             type: 'GET',
@@ -31,21 +28,20 @@ var index = new Vue({
             success: function(rsp) {
               eval('index.db = ' + rsp);
               console.log('You are now using the local database');
+              console.log(index.db);
               index.db_empty = false;
             }
           });
-          console.log('ajax error!');
         }
       });
 
-      //next initiation
+      //next function...
 
     },
 
     test: function(){
-      if (index.db_empty) {
-        console.log(index.db);
-        console.log('database github version loaded fail');
+      if (!index.db_empty) {
+        console.log('db_empty is '+index.db_empty);
       };
     },
 
@@ -55,6 +51,7 @@ var index = new Vue({
 })
 
 index.init();
-index.test();
 
-//var t = function()｛document.documentElement.scrollTop=0｝
+setTimeout(function(){
+  index.test();
+},1000);
