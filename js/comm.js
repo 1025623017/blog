2c79efeccbcf3f1a6a71e84eceac884bbad9215a
Murphy Leung <1025623017@qq.com>
2023-12-15 23:39:37 +0800

    2023年12月15日 23:39:36

--- js/comm.js
+++ js/comm.js
@@ -3,7 +3,7 @@ var index = new Vue({
   data: {
     //空的本地数据库
     db: {},
-    //是否获取到Ajax数据库
+    //获取Ajax数据库失败标识
     db_empty: true
   },
   methods: {
@@ -18,47 +18,36 @@ var index = new Vue({
         async: false,
         //获取网络数据库
         success: function(rsp) {
-          console.log(rsp.status);
-          setTimeout(()=>{
-              eval('index.db = ' + rsp);
-              index.db_empty = true;
-              console.log('Database Github Version loaded successful');
-          },0);
+          eval('index.db = ' + rsp);
+          index.db_empty = false;
+          console.log('Database Github Version loaded successful');
         },
         //获取本地数据库
         error: function(rsp) {
-          console.log('ajax error!');
-          console.log(rsp.status);
-          index.db_empty = false;
-          //alert('index.db_empty = false;');
           $.ajax({
             url: 'https://1025623017.github.io/blog/db/db',
             type: 'GET',
             async: false,
             success: function(rsp) {
-              setTimeout(()=>{
-                try {
-                  eval('index.db = ' + rsp);
-                  console.log('You are using the local database');
-                  index.db_empty = true;
-                } catch (error){
-                  console.log(error);
-                }
-              },0);
+              eval('index.db = ' + rsp);
+              console.log('You are now using the local database');
+              index.db_empty = false;
             }
           });
+          console.log('ajax error!');
         }
       });
 
-
       //next initiation
+
     },
 
-    test: function(log){
-      console.log(
-        index.db
-      )
-    }
+    test: function(){
+      if (index.db_empty) {
+        console.log(index.db);
+        console.log('database github version loaded fail');
+      };
+    },
 
     //---
 
--- js/test.js
+++ js/test.js
@@ -1,25 +1,15 @@
-var test = new Vue({
-  el: '#index',
+var database_list = new Vue({
+  el: '#database_list',
   data: {
-  	excel_list:{
-  		a:1,
-  		b:2
-  	},
-  	ppt_list:{}
+  	db_refresh: {}
   },
   methods: {
 
-    getData: function(a){
-      console.log(test.excel_list);
-      console.log("loading excel data...");
-    },
-
-    postData: function(b){
-      console.log(test.ppt_list);
-      console.log("loading ppt 01...");
+    doRefresh: function(){
+      db_refresh = index.db;
     }
 
   }
 })
 
-//test.getData();
+database_list.doRefresh();
