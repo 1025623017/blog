aa0c9f4be56fb7022087d8749e519b30e0922f81
Murphy Leung <1025623017@qq.com>
2023-12-17 14:25:24 +0800

    2023年12月17日 14:25:23

--- js/comm.js
+++ js/comm.js
@@ -38,7 +38,7 @@ var index = new Vue({
           success: function(rsp) {
             index.db = eval('index.db = ' + rsp);
             if(!window.location.href.includes('file')){
-              console.log('2023年12月17日 13:45:08');
+              //console.log('');
             }
           },
           error: function(rsp) {
